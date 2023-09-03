b2c7fca3ba538496a67c8f164695990efc9738e1
G-Cuevas <cuevasgilberto118@gmail.com>
2023-09-03 13:51:18 -0700

    counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Counter</h2>
+  <button @click="increment">{{counter}}</button>
+  <div>
+    o
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+
+    data() {
+        return {
+            counter: 0
+        }
+    },
+
+    methods: {
+        increment() {
+            this.counter++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
